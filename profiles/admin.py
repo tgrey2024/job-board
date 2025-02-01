c328a482c6ad857139bf2a9edcb7fbb88acbd3f3
tgrey2024 <tripta.grey@gmail.com>
2025-02-01 13:02:12 +0000

    update job board and profiles models and admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
-from .models import ApplicantProfile, EmployerProfile
+from .models import Applicant, Employer
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(ApplicantProfile)
-class ApplicantProfileAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'content')
-    search_fields = ['title']
+@admin.register(Employer)
+class EmployerAdmin(SummernoteModelAdmin):
+
+    list_display = ('company_name', 'content')
+    search_fields = ['company_name']
     summernote_fields = ('content',)
 
+@admin.register(Applicant)
+class ApplicantAdmin(SummernoteModelAdmin):
+    list_display = ('firstname', 'lastname', 'role', 'updated_on')
+    search_fields = ['firstname', 'lastname', 'role']
+    summernote_fields = ('intro','skills','experience')
 
-@admin.register(EmployerProfile)
-class EmployerProfileAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'content')
-    search_fields = ['title']
-    summernote_fields = ('content',)
--- profiles/models.py
+++ profiles/models.py
@@ -1,23 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
-class EmployerProfile(models.Model):
-    title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+class Employer(models.Model):
+    company_name = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    logo = CloudinaryField('image', default='placeholder')
+    site_url = models.URLField(blank=True)
+    address1 = models.TextField(blank=True)
+    address2 = models.TextField(blank=True)
+    city = models.CharField(max_length=200)
+    postcode = models.CharField(max_length=200)
     employer = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 
-class ApplicantProfile(models.Model):
-    title = models.CharField(max_length=200)
+class Applicant(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+    intro = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    profilepic = CloudinaryField('image', default='placeholder')
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200)
+    postcode = models.CharField(max_length=200)
+    skills = models.TextField(blank=True)
+    experience = models.TextField(blank=True)
     applicant = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
