f89a43d026a1d7f23772adb14510694ee857428c
tgrey2024 <tripta.grey@gmail.com>
2025-02-03 10:49:01 +0000

    install crispyforms and add applicant profile form

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import EmployerListView, Profiles, ApplicantListView, ApplicantProfile
+from .views import EmployerListView, Profiles, ApplicantListView
+from .views import ApplicantProfile, ApplicantUpdateView
 
 urlpatterns = [
     path('employers/', EmployerListView.as_view(), name='employer_list'),
     path('employer/<int:user_id>/', Profiles.as_view(), name='employer_profile'),
     path('applicants/', ApplicantListView.as_view(), name='applicant_list'),
     path('applicant/<int:user_id>/', ApplicantProfile.as_view(), name='applicant_profile'),
-    path('profile/<int:user_id>/', Profiles.as_view(), name='profile'),
+    path('profile/<int:user_id>/', ApplicantProfile.as_view(), name='profile'),
+    # path('applicant/create/', ApplicantCreateView.as_view(), name='applicant_create'),
+    path('applicant/update/<int:pk>/', ApplicantUpdateView.as_view(), name='applicant_update'),
 ]
